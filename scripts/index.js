f58096789c6f1365e33cd5c329c26dab8e39475e
FunkyMuffin <omer.roth@ymail.com>
2017-10-29 16:57:54 +0200

    made testing group

--- scripts/index.js
+++ scripts/index.js
@@ -7,38 +7,38 @@ var posts = [
 ];
 
 function populateFeed() {
-	var feed = document.getElementById("feed");
+	var feed = document.getElementById('feed');
 	for (var i = 0; i < posts.length; i++) {
-		var df = createPostDiv(posts[i].username,posts[i].text);
+		var df = createPostDiv(posts[i].username, posts[i].text);
 		feed.appendChild(df);
 	}
 }
 
 function addTweet() {
-	var textBox = document.getElementById("tweetBox");
-	var feed = document.getElementById("feed");
+	var textBox = document.getElementById('tweetBox');
+	var feed = document.getElementById('feed');
 
-	var df = createPostDiv("someone",textBox.value);
+	var df = createPostDiv('someone', textBox.value);
 	feed.appendChild(df);
-	posts.push({username: '11', test: textBox.value});
-	textBox.value = "";
+	//posts.push({username: '11', test: textBox.value});
+	textBox.value = '';
 }
 
-function createPostDiv(person,text) {
+function createPostDiv(person, text) {
 	var df = document.createDocumentFragment();
 	var div = document.createElement('div');
-	div.className = "row post";
+	div.className = 'row post';
 
 	var ul = document.createElement('ul');
 	var img = document.createElement('img');
-	img.src = "../images/useravatar_preview.png";
+	img.src = '../images/useravatar_preview.png';
 
 	var li1 = document.createElement('li');
 	var b = document.createElement('b');
-	b.innerText = person + ":";
+	b.innerText = person + ':';
 
 	var li2 = document.createElement('li');
-	li2.innerText= text;
+	li2.innerText = text;
 
 	df.appendChild(div);
 	div.appendChild(img);
@@ -47,4 +47,55 @@ function createPostDiv(person,text) {
 	li1.appendChild(b);
 	ul.appendChild(li2);
 	return df;
+}
+
+var unitsDiv;
+
+function assert(result,name) {
+	var testP = document.createElement('p');
+	testP.innerHTML = name;
+
+	if (result) {
+		testP.className = "alert alert-success";
+		testP.innerHTML = testP.innerHTML + " Success";
+	}
+	else  {
+		testP.className = "alert alert-danger";
+		testP.innerHTML = testP.innerHTML + " Failed";
+	}
+	unitsDiv.appendChild(testP);
+}
+
+function test_group(name, tests) {
+	var df = document.createDocumentFragment();
+
+	var row = document.createElement('div');
+	row.className = 'row col-xs-offset-2 col-xs-8';
+	var div = document.createElement('div');
+	div.style.textAlign = "center";
+	div.className = "panel panel-default";
+	var title = document.createElement('h2');
+	title.innerHTML = name;
+	unitsDiv = document.createElement('div');
+	unitsDiv.className ="panel panel-default";
+	unitsDiv.style.backgroundColor = "transparent";
+
+	div.appendChild(title);
+	div.appendChild(unitsDiv);
+	row.appendChild(div);
+	df.appendChild(row);
+	document.getElementsByTagName('body')[0].appendChild(df);
+
+	tests();
+	var children = unitsDiv.getElementsByTagName('p');
+
+	for (var i = 0; i < children.length; i++) {
+		if (children[i].innerHTML.toUpperCase().indexOf("FAILED") != -1) {
+			div.style.backgroundColor = "#ef4438";
+			return;
+		}
+		else {
+			div.style.backgroundColor = "green";
+		}
+	}
 }
